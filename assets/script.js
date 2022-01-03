12f610784df5aed29257b845dde8d90d663c368d
Akeia M <mccollumakeia@outlook.com>
2022-01-03 16:25:14 -0500

    added html, added js

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+var api = "68f441a7850cce87d500e391411afce2"
+    var makeItRain = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
+    var latLong = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
+
+    // Function
+function 
