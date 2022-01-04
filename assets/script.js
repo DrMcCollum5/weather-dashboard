94942b06de10696b43a22d844b6dc8d3b9842ae8
Akeia M <mccollumakeia@outlook.com>
2022-01-04 09:36:24 -0500

    added function and json format

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,31 @@
 var api = "68f441a7850cce87d500e391411afce2"
-    var makeItRain = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
-    var latLong = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
+
+
 
     // Function
-function 
+function getLatLong () {
+    var latLong = "https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=68f441a7850cce87d500e391411afce2"
+    //1. Call to the api url
+    fetch (latLong)
+    //2. Ask for the data to use JSON format
+    .then(function(res){
+        return res.json();
+    }).then(function(data){
+        // 3. ACTION - anything you want to do with the data, do it here!
+
+        console.log("LATITUDE: ", data.coord.lat);
+
+        getforecast(data.coord.lat, data.coord.lon)
+    })
+};
+function getforecast (lat, lon) {
+    var makeItRain = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=68f441a7850cce87d500e391411afce2`;
+    fetch (makeItRain)
+    .then(function(res){
+        return res.json ();
+        })
+        .then (function (data){
+console.log (data)
+        })
+}
+getLatLong()
