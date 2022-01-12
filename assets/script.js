b358539912db20893f7156d1d005b0418f2bc86c
Akeia M <mccollumakeia@outlook.com>
2022-01-12 09:32:50 -0500

    created an image element, append forcast card, create temp card

--- assets/script.js
+++ assets/script.js
@@ -145,4 +145,66 @@ var display5Day = function(weather){
        forecastEl.classList = "card bg-primary text-light m-2";
 
        //console.log(dailyForecast)
-       
+      //create date element
+      var forecastDate = document.createElement("h5")
+      forecastDate.textContent= moment.unix(dailyForecast.dt).format("MMM D, YYYY");
+      forecastDate.classList = "card-header text-center"
+      forecastEl.appendChild(forecastDate);
+
+      
+      //create an image element
+      var weatherIcon = document.createElement("img")
+      weatherIcon.classList = "card-body text-center";
+      weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${dailyForecast.weather[0].icon}@2x.png`);  
+
+      //append to forecast card
+      forecastEl.appendChild(weatherIcon);
+      
+      //create temperature span
+      var forecastTempEl=document.createElement("span");
+      forecastTempEl.classList = "card-body text-center";
+      forecastTempEl.textContent = dailyForecast.main.temp + " °F";
+
+       //append to forecast card
+       forecastEl.appendChild(forecastTempEl);
+
+      var forecastHumEl=document.createElement("span");
+      forecastHumEl.classList = "card-body text-center";
+      forecastHumEl.textContent = dailyForecast.main.humidity + "  %";
+
+      //append to forecast card
+      forecastEl.appendChild(forecastHumEl);
+
+       // console.log(forecastEl);
+      //append to five day container
+       forecastContainerEl.appendChild(forecastEl);
+   }
+
+}
+
+var pastSearch = function(pastSearch){
+
+   // console.log(pastSearch)
+
+   pastSearchEl = document.createElement("button");
+   pastSearchEl.textContent = pastSearch;
+   pastSearchEl.classList = "d-flex w-100 btn-light border p-2";
+   pastSearchEl.setAttribute("data-city",pastSearch)
+   pastSearchEl.setAttribute("type", "submit");
+
+   pastSearchButtonEl.prepend(pastSearchEl);
+}
+
+
+var pastSearchHandler = function(event){
+   var city = event.target.getAttribute("data-city")
+   if(city){
+       getCityWeather(city);
+       get5Day(city);
+   }
+}
+
+// pastSearch();
+
+cityFormEl.addEventListener("submit", formSumbitHandler);
+pastSearchButtonEl.addEventListener("click", pastSearchHandler);
